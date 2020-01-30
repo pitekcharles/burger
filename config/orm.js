4cc3fdbc7ec6162c219652d2ca0967c07d4fe83d
Noodles <charles.pitek@gmail.com>
2020-01-30 12:18:20 -0700

    started setting up server.js file, a little bit of work on the orm.

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,23 @@
 var connection = require("./connection.js");
 
+var orm = {
+    selectAll: function (tableInput, cb){
+        var queryString = "SELECT * FROM ??";
+        connection.query(queryString, [tableInput], function(err, result){
+            if (err) throw err;
+            return cb(result);
+        });
+    },
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = `INSERT INTO ?? (${cols.join(",")}) VALUES (${vals.join(",")})`;
+        connection.query(queryString, table, function(err, result){
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    updateOne: function (){
+
+    }
+};
+
+module.exports = orm;
