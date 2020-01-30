bf8277af070a7b4df2a113602c24d4b3f3a4af3c
Noodles <charles.pitek@gmail.com>
2020-01-30 09:43:17 -0700

    filled in the seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_names) VALUES ("Cheese Burger");
+INSERT INTO burgers (burger_names) VALUES ("Mushroom Swiss burger");
+INSERT INTO burgers (burger_names) VALUES ("Impossible Burger");
