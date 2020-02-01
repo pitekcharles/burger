119d95247347c3aadcbeaad4d9fd224ece8b19d8
Noodles <charles.pitek@gmail.com>
2020-02-01 13:49:02 -0700

    changed code for remote db setup

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Xiyuan18",
-    database: "burgers_db"
-});
+var connection;
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Xiyuan18",
+        database: "burgers_db"
+    });
+}
 
 connection.connect(function (error){
     if (error) {
